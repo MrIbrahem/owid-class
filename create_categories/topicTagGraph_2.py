1a8890a8974a19ccf657b6fafd83eba15df7da70
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-03-18 03:56:19 +0300

    Update topicTagGraph_2.py

--- create_categories/topicTagGraph_2.py
+++ create_categories/topicTagGraph_2.py
@@ -73,14 +73,11 @@ def create_category_text(main_category, category, sub_categories) -> str:
     text = []
 
     text.append(f"[[:Category:Our World in Data topics|Our World in Data topics]] > [[:Category:Our World in Data - {main_category}|{main_category}]] > [[:Category:Our World in Data - {category}|{category}]]:")
-    text.append("Topics in this category:")
-
-    category_text = slug_link(category)
-    text.append(f"* {category_text}")
+    text.append("\nTopics in this category:")
 
     for x in sub_categories:
         x_text = slug_link(x)
-        text.append(f"** {x_text}")
+        text.append(f"* {x_text}")
 
     text.append(f"[[Category:Our World in Data - {main_category}| ]]")
 
